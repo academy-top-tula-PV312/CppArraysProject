008c959267d85beefb255abc262076bd24224341
Максим Ефимов <efimov_m@t.top-academy.ru>
2023-12-25 21:14:06 +0300

    Добавьте файлы проекта.

---
+++ CppArraysProject/CppArraysProject.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+//void Func(int array[])
+//{
+//    array[0] = 100;
+//}
+
+void ArrayPrint(int array[], int size)
+{
+    for (int i{}; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
+}
+
+void ArrayInit(int array[], int size, int begin, int end);
+
+void ArrayInit(int array[], int size, int end)
+{
+    for (int i{}; i < size; i++)
+        array[i] = rand() % (end + 1);
+}
+
+
+int main()
+{
+    //std::cout << time(nullptr) << "\n";
+    srand(time(nullptr));
+
+    const int size{ 5 };
+    int students[size];
+
+    ArrayInit(students, size, 12);
+
+    ArrayPrint(students, size);
+
+    int summa{};
+    for (int i{}; i < size; i++)
+        summa += (students[i] + 1);
+
+    std::cout << (double)summa / size << "\n";
+}
